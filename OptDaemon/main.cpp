1e91d5610b3a5cd34f2b83d77a7abfdf1a659b1a
Lance Colton <lance.colton@gmail.com>
2012-04-20 01:58:43 -0600

    should really work... (but it doesn't verify still)

--- OptDaemon/main.cpp
+++ OptDaemon/main.cpp
@@ -9,24 +9,29 @@
 
 
 static const char AC_MODULE_HASH[] = {
+//    0xac,0x34,0xf7,0x5,0x79,0x1d,0x4f,0x6,0xfd,0xf0,0x5e,0xd,0x14,0x29,0xb3,0x7a,0x80,0xf6,0x3b,0xbf
 #include "../qtc_packaging/debian_harmattan/symsmodhash.inc"
 };
 
 static const char AC_MODULE2_HASH[] = {
 #include "../qtc_packaging/debian_harmattan/opptmodhash.inc"
+  //  0x9c,0xd3,0xda,0xb0,0x90,0x2e,0x65,0xe4,0xfe,0xa9,0x41,0xba,0x7,0x6b,0xff,0x95,0xe,0x51,0xde,0xab
 };
 
 int whitelist_module(const char hash[SHA1_HASH_LENGTH])
 {
     QFile file(AC_MODLIST_PATH);
-    if (! file.open(QIODevice::WriteOnly | QIODevice::Append)){
+    if (! file.open(QIODevice::WriteOnly | QIODevice::Truncate)){
         qDebug() << "modlist file open failed!!";
         qDebug() << file.errorString();
         return 0;
     }
-    QDataStream out(&file);
-    out.writeBytes(hash,SHA1_HASH_LENGTH);
-    return 1;
+    //QDataStream out(&file);
+    //int ret = out.writeRawData(hash,SHA1_HASH_LENGTH);
+    int ret = file.write(hash,SHA1_HASH_LENGTH);
+    qDebug() << file.errorString();
+    file.close();
+    return ret;
 }
 
 int main(int argc, char *argv[])
@@ -45,40 +50,49 @@ int main(int argc, char *argv[])
     else
         qDebug() << "modlist file opened successfully";
 
-    //QDataStream in(&file);
     QByteArray hashlist;
     hashlist = file.readAll();
-    //in >> hashlist;
-
-    qDebug() << hashlist;
 
     QByteArray NewHash;
-    NewHash.fromRawData(AC_MODULE_HASH,sizeof(AC_MODULE_HASH));
+    NewHash = QByteArray::fromRawData(AC_MODULE_HASH,sizeof(AC_MODULE_HASH));
 
-    qDebug() << NewHash;
+    //qDebug() << sizeof(AC_MODULE_HASH);
+    //qDebug() << NewHash.toHex();
 
-    if (!hashlist.contains(NewHash)){
+    if (!hashlist.contains(NewHash.toHex())){
         NeedWhitelisting1 = true;
-        qDebug() << "hash1 not found";
+        qDebug() << "hash1 not found in whitelist";
     }
+    else
+        qDebug() << "hash1 already whitelisted";
 
     QByteArray NewHash2;
-    NewHash2.fromRawData(AC_MODULE2_HASH,sizeof(AC_MODULE2_HASH));
-    if (!hashlist.contains(NewHash2)){
+    NewHash2  = QByteArray::fromRawData(AC_MODULE2_HASH,sizeof(AC_MODULE2_HASH));
+    if (!hashlist.contains(NewHash2.toHex())){
         NeedWhitelisting2 = true;
-        qDebug() << "hash2 not found";
+        qDebug() << "hash2 not found in whitelist";
     }
+    else
+        qDebug() << "hash2 already whitelisted";
 
     //whitelist modules
     if (NeedWhitelisting1){
         int ret = whitelist_module(AC_MODULE_HASH);
-        if (ret == 0)
+        if (ret < 1){
+             qDebug() << "hash1 insert failed";
              return app.exec();
+        }
+        else
+            qDebug() << "wrote hash1: " << ret << "bytes";
     }
     if (NeedWhitelisting2){
         int ret = whitelist_module(AC_MODULE2_HASH);
-        if (ret == 0)
+        if (ret < 1){
+             qDebug() << "hash2 insert failed";
              return app.exec();
+        }
+        else
+            qDebug() << "wrote hash2: " << ret << "bytes";
     }
 
     QProcess p;
